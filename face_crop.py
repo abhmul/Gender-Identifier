318bba24205d9525ac21d9ae333112d49cc202da
abhmul <abhmul@gmail.com>
2016-05-15 12:54:54 -0500

    changed min neighbors back to 5

--- face_crop.py
+++ face_crop.py
@@ -40,7 +40,7 @@ for file in files:
             faces = faceCascade.detectMultiScale(
                 gray,
                 scaleFactor=scale_factor,
-                minNeighbors=10,
+                minNeighbors=5,
                 minSize=(30, 30)
                 # flags = cv2.cv.CV_HAAR_SCALE_IMAGE
             )
