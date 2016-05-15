ae0fadd62faaa858b3e7abe3773dc6f64489e61f
abhmul <abhmul@gmail.com>
2016-05-15 11:35:53 -0500

    Fixed a typo

--- face_crop.py
+++ face_crop.py
@@ -7,7 +7,7 @@ cascPath = "haarcascade_frontalface_default.xml"
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-files=glob.glob("../*.jpg")
+files=glob.glob("*.jpg")
 for file in files:
 
     # Read the image
