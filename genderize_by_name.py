def0e8cb22ff064e43c83467ad40d47f3ee35e78
abhmul <abhmul@gmail.com>
2016-06-07 20:52:03 -0500

    Fixed Genderize script

--- genderize_by_name.py
+++ genderize_by_name.py
@@ -47,10 +47,9 @@ def build_gender_text(genderedListMaledir, genderedListFemaledir, maleFolder, fe
 
 
 
-def genderize(fileNamesdir, genderedListMaledir, genderedListFemaledir, ungenderedListdir):
+def genderize(fileNamesdir, genderedListMaledir, genderedListFemaledir, ungenderedListdir, threshold=0.9):
 
     count = 0
-    tmp = ""
 
     genderedListMale = open(genderedListMaledir, 'r')
     genderedListFemale = open(genderedListFemaledir, 'r')
@@ -59,6 +58,10 @@ def genderize(fileNamesdir, genderedListMaledir, genderedListFemaledir, ungender
     listMale = genderedListMale.read().split("\n")
     listFemale = genderedListFemale.read().split("\n")
     listUngendered = ungenderedList.read().split("\n")
+
+    maleNames = set([(fileName.split("_"))[0] for fileName in listMale])
+    femaleNames = set([(fileName.split("_"))[0] for fileName in listFemale])
+    ungenderedNames = set([(fileName.split("_"))[0] for fileName in listUngendered])
 
     genderedListMale.close()
     genderedListFemale.close()
@@ -78,32 +81,61 @@ def genderize(fileNamesdir, genderedListMaledir, genderedListFemaledir, ungender
         if file not in fileSet:
             count += 1
 
-            if tmp != fileSplit[0]:
-                result = requests.get("http://api.genderize.io?name=%s" % fileSplit[0])
-                result = result.json()
-                tmp = fileSplit[0]
-            else:
-                tmp = fileSplit[0]
-
             genderedListMale = open(genderedListMaledir, 'a')
             genderedListFemale = open(genderedListFemaledir, 'a')
             ungenderedList = open(ungenderedListdir, 'a')
 
-            try:
-                if float(result['probability']) > 0.9:
-                    if result['gender'] == 'male':
-                        genderedListMale.write(file + "\n")
-                    elif result['gender'] == 'female':
-                        genderedListFemale.write(file + "\n")
-                    fileSet.add(file)
-            except Exception as e:
-                if 'error' in result.keys():
-                    print result['error']
-                    return None
-                print result['name']
-                ungenderedList.write(file + "\n")
 
-            print count
+
+            if fileSplit[0] in maleNames:
+                genderedListMale.write(file + "\n")
+                print fileSplit[0], " placed in MALES"
+                print "No Call"
+
+            elif fileSplit[0] in femaleNames:
+                genderedListFemale.write(file + "\n")
+                print fileSplit[0], " placed in FEMALES"
+                print "No Call"
+            elif fileSplit[0] in ungenderedNames:
+                ungenderedList.write(file + "\n")
+                print fileSplit[0], " placed in UNGENDERED"
+                print "No Call"
+            else:
+                result = requests.get("http://api.genderize.io?name=%s" % fileSplit[0])
+                print result.headers
+                result = result.json()
+                print "API was called for ", fileSplit[0]
+                if 'error' not in result.keys() and 'gender' in result.keys():
+                    print result['probability'], "  ", result['gender']
+
+                    if float(result['probability']) > threshold:
+                        if result['gender'] == 'male':
+                            genderedListMale.write(file + "\n")
+                            maleNames.add(fileSplit[0])
+                            print fileSplit[0], " placed in MALES"
+
+                        elif result['gender'] == 'female':
+                            genderedListFemale.write(file + "\n")
+                            femaleNames.add(fileSplit[0])
+                            print fileSplit[0], " placed in FEMALES"
+                else:
+                    try:
+                        print result['name']
+                        ungenderedList.write(file + "\n")
+                        ungenderedNames.add(fileSplit[0])
+                        print fileSplit[0], " placed in UNGENDERED"
+
+                    except:
+                        print result['error']
+                        return None
+
+
+
+            fileSet.add(file)
+
+            print count, "\n"
+            # if count > 1:
+            #     return None
 
     genderedListMale.close()
     genderedListFemale.close()
